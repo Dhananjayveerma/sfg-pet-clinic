e1d979cec1b9c05a4c182bfe86469c512d55d5a8
LAPTOP-NGF9F7C6\Dhananjay Verma <ncerteducation@gmail.com>
2018-10-31 23:22:36 +0530

    ok

---
+++ pet-clinic-data/src/main/java/drv/springfrmework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package drv.springfrmework.sfgpetclinic.services;
+
+import drv.springfrmework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long> {
+}
