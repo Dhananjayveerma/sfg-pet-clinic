2c87863e8ee11f0506aee8938fa724458c93e7bb
LAPTOP-NGF9F7C6\Dhananjay Verma <ncerteducation@gmail.com>
2018-11-19 23:27:02 +0530

    ok

--- pet-clinic-data/src/main/java/drv/springfrmework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/drv/springfrmework/sfgpetclinic/model/Person.java
@@ -1,11 +1,17 @@
 package drv.springfrmework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 /*
 * Created by it 2018/09/12
 * */
+@MappedSuperclass
 public class Person extends BaseEntity{
 
+    @Column(name="first_name")
     private String firstName;
+    @Column(name="last_name")
     private String lastName;
 
     public String getFirstName() {
